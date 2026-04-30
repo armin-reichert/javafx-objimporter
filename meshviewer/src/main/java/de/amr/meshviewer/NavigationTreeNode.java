c31fad014e58645bf00dfa5258467af0e6ce6a69
Armin Reichert <armin.reichert@web.de>
2026-04-30 12:51:54 +0200

    New repository

---
+++ meshviewer/src/main/java/de/amr/meshviewer/NavigationTreeNode.java
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2021-2026 Armin Reichert (MIT License)
+ */
+
+package de.amr.meshviewer;
+
+public sealed class NavigationTreeNode permits LabelNode, MeshNode {
+}
---
+++ objparser/src/main/java/de/amr/objparser/ObjTexCoord.java
@@ -0,0 +1,7 @@
+/*
+ * Copyright (c) 2021-2026 Armin Reichert (MIT License)
+ */
+
+package de.amr.objparser;
+
+public record ObjTexCoord(float u, float v) {}
