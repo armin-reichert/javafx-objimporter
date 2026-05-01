c85149a3cae901e7cabec3b9986aacaea1b85281
Armin Reichert <armin.reichert@web.de>
2026-05-01 21:49:28 +0200

    Improved

--- meshviewer/src/main/java/de/amr/samples/ScooterDemoApp.java
+++ meshviewer/src/main/java/de/amr/samples/ScooterDemoApp.java
@@ -24,41 +24,87 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 /**
- * Derived from <a href="https://github.com/AlmasB/javafx3d-samples/blob/master/src/main/java/com/almasb/fx3d/modelloading/ModelLoadApp.java">Almas B's sample app</a>
+ * Derived from
+ * <a href="https://github.com/AlmasB/javafx3d-samples/blob/master/src/main/java/com/almasb/fx3d/modelloading/ModelLoadApp.java">Almas B's sample app</a>
  */
 public class ScooterDemoApp extends Application {
 
-    public static final double AUTO_ROTATE_SPEED = 0.2;
+    public static final double AUTO_ROTATE_SPEED = 0.1;
 
+    public static final String DRAW_MODE_TOGGLE = "d";
+    public static final String SCOOTER_ROTATION_TOGGLE = "r";
+    public static final String WHEELS_ROTATION_TOGGLE = "w";
+
+    private Group scooter3D;
     private PerspectiveCamera camera;
+
+    private ParallelTransition wheelsAnimation;
+
     private Animation autoRotateAnimation;
     private final Rotate autoRotateY = new Rotate(0, Rotate.Y_AXIS);
 
     private final ObjectProperty<DrawMode> drawMode = new SimpleObjectProperty<>(DrawMode.FILL);
 
-    private Scene createScene() throws IOException {
+    @Override
+    public void start(Stage stage) throws Exception {
+        createScooter3D();
+        stage.setScene(createScene());
+        stage.setTitle("Scooter (Courtesy of Almas B)");
+        createAutoRotateAnimation();
+        createScooterWheelsAnimation();
 
+        autoRotateAnimation.play();
+        wheelsAnimation.play();
+
+        stage.show();
+    }
+
+    private void createScooter3D() throws IOException {
+        scooter3D = new Group();
+        final URL url = getClass().getResource("/scooter/Scooter-smgrps.obj");
+        if (url != null) {
+            final ObjModel objModel = new ObjFileParser(url, StandardCharsets.UTF_8).parse();
+            for (MeshView part : MeshBuilder.build(objModel, MeshBuilder.BuildMode.BY_GROUP).values()) {
+                scooter3D.getChildren().add(part);
+                part.drawModeProperty().bind(drawMode);
+            }
+            scooter3D.getTransforms().add(new Rotate(90, Rotate.Y_AXIS));
+            scooter3D.getTransforms().add(new Rotate(180, Rotate.X_AXIS));
+            scooter3D.getTransforms().add(autoRotateY);
+            // move a bit down
+            scooter3D.getTransforms().add(new Translate(0, -0.4, 0));
+        }
+    }
+
+    private Scene createScene() {
         camera = new PerspectiveCamera(true);
         camera.setTranslateZ(-2.5);
 
-        Group scooter = loadModel(getClass().getResource("/scooter/Scooter-smgrps.obj"));
-        scooter.getTransforms().add(new Rotate(90, Rotate.Y_AXIS));
-        scooter.getTransforms().add(new Rotate(180, Rotate.X_AXIS));
-        scooter.getTransforms().add(autoRotateY);
-        // move a bit down
-        scooter.getTransforms().add(new Translate(0, -0.4, 0));
+        final Group root = new Group(scooter3D);
 
-        animate(scooter);
-
-        Group root = new Group(scooter);
-
-        Scene scene = new Scene(root, 800, 600, true, SceneAntialiasing.BALANCED);
+        final Scene scene = new Scene(root, 800, 600, true, SceneAntialiasing.BALANCED);
         scene.setFill(Color.LIGHTBLUE);
         scene.setCamera(camera);
 
         scene.setOnKeyTyped(e -> {
-            if ("w".equals(e.getCharacter())) {
+            if (DRAW_MODE_TOGGLE.equals(e.getCharacter())) {
                 drawMode.set(drawMode.get() == DrawMode.FILL ? DrawMode.LINE : DrawMode.FILL);
+            }
+            else if (SCOOTER_ROTATION_TOGGLE.equals(e.getCharacter())) {
+                boolean running = autoRotateAnimation.getStatus() == Animation.Status.RUNNING;
+                if (running) {
+                    autoRotateAnimation.pause();
+                }  else {
+                    autoRotateAnimation.play();
+                }
+            }
+            else if (WHEELS_ROTATION_TOGGLE.equals(e.getCharacter())) {
+                boolean running = wheelsAnimation.getStatus() == Animation.Status.RUNNING;
+                if (running) {
+                    wheelsAnimation.pause();
+                }  else {
+                    wheelsAnimation.play();
+                }
             }
         });
 
@@ -69,9 +115,7 @@ public class ScooterDemoApp extends Application {
             }
         });
 
-        scene.setOnScroll(e -> {
-            zoomBy(e.getDeltaY() * 0.01);
-        });
+        scene.setOnScroll(e -> zoomBy(e.getDeltaY() * 0.01));
 
         return scene;
     }
@@ -80,30 +124,18 @@ public class ScooterDemoApp extends Application {
         camera.setTranslateZ(camera.getTranslateZ() + amount);
     }
 
-    private Group loadModel(URL url) throws IOException {
-        Group modelRoot = new Group();
-
-        ObjModel objModel = new ObjFileParser(url, StandardCharsets.UTF_8).parse();
-
-        for (MeshView view : MeshBuilder.build(objModel, MeshBuilder.BuildMode.BY_GROUP).values()) {
-            modelRoot.getChildren().add(view);
-            view.drawModeProperty().bind(drawMode);
-        }
-
-        return modelRoot;
-    }
-
-    private void animate(Group model) {
-        model.getChildren()
+    private void createScooterWheelsAnimation() {
+        wheelsAnimation = new ParallelTransition();
+        scooter3D.getChildren()
             .stream()
-            .filter(view -> view.getId().endsWith(".RimFront") || view.getId().endsWith(".RimRear"))
-            .forEach(view -> {
-                RotateTransition rt = new RotateTransition(Duration.seconds(0.33), view);
-                rt.setCycleCount(Integer.MAX_VALUE);
+            .filter(part -> part.getId().endsWith(".RimFront") || part.getId().endsWith(".RimRear"))
+            .forEach(part -> {
+                RotateTransition rt = new RotateTransition(Duration.seconds(0.33), part);
+                rt.setCycleCount(Animation.INDEFINITE);
                 rt.setAxis(Rotate.X_AXIS);
                 rt.setByAngle(360);
                 rt.setInterpolator(Interpolator.LINEAR);
-                rt.play();
+                wheelsAnimation.getChildren().add(rt);
             });
     }
 
@@ -113,14 +145,5 @@ public class ScooterDemoApp extends Application {
         );
         autoRotateAnimation.setCycleCount(Animation.INDEFINITE);
 
-    }
-
-    @Override
-    public void start(Stage stage) throws Exception {
-        stage.setScene(createScene());
-        stage.setTitle("Scooter (Courtesy of Almas B)");
-        stage.show();
-        createAutoRotateAnimation();
-        autoRotateAnimation.play();
     }
 }
